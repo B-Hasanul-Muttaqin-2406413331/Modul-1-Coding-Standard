7df541d4b3bc20a29fb331ebc018d5fc93134e51
hasanul muttaqin <hasanul.muttaqin@ui.ac.id>
2026-03-03 08:47:15 +0700

    add: car repository

--- src/main/java/id/ac/ui/cs/advprog/eshop/model/Car.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/Car.java
@@ -1,4 +1,14 @@
 package id.ac.ui.cs.advprog.eshop.model;
+import lombok.Getter;
+import lombok.Setter;
 
+import java.util.UUID;
+
+@Getter
+@Setter
 public class Car {
+    private UUID carID;
+    private String carName;
+    private String carColor;
+    private int carQuantity;
 }

5721d9da20c0c703814b490276e632d581e08b0c
hasanul muttaqin <hasanul.muttaqin@ui.ac.id>
2026-03-10 19:51:37 +0700

    [GREEN] Implement PaymentRepository

--- src/main/java/id/ac/ui/cs/advprog/eshop/repository/PaymentRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/repository/PaymentRepository.java
@@ -9,15 +9,33 @@ import java.util.List;
 @Repository
 public class PaymentRepository {
 
+    private List<Payment> paymentData = new ArrayList<>();
+
     public Payment save(Payment payment) {
-        return null;
+        int i = 0;
+        for (Payment savedPayment : paymentData) {
+            if (savedPayment.getId().equals(payment.getId())) {
+                paymentData.remove(i);
+                paymentData.add(i, payment);
+                return payment;
+            }
+            i += 1;
+        }
+
+        paymentData.add(payment);
+        return payment;
     }
 
     public Payment findById(String paymentId) {
+        for (Payment savedPayment : paymentData) {
+            if (savedPayment.getId().equals(paymentId)) {
+                return savedPayment;
+            }
+        }
         return null;
     }
 
     public List<Payment> getAllPayments() {
-        return new ArrayList<>();
+        return paymentData;
     }
 }

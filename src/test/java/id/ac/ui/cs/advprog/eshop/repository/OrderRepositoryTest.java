8722364ad1c567441d9215d0b513d19079c342da
hasanul muttaqin <hasanul.muttaqin@ui.ac.id>
2026-03-10 16:41:31 +0700

    [RED] Add OrderRepository skeleton

--- src/test/java/id/ac/ui/cs/advprog/eshop/repository/OrderRepositoryTest.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/repository/OrderRepositoryTest.java
@@ -10,7 +10,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 class OrderRepositoryTest {
 

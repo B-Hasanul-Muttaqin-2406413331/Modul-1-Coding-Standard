00b4743ca37aa4b3a8ac95bf07d89d5259836f6a
hasanul muttaqin <hasanul.muttaqin@ui.ac.id>
2026-03-10 20:37:31 +0700

    [GREEN] Implement OrderController

--- src/main/java/id/ac/ui/cs/advprog/eshop/controller/OrderController.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/controller/OrderController.java
@@ -1,12 +1,42 @@
 package id.ac.ui.cs.advprog.eshop.controller;
 
+import id.ac.ui.cs.advprog.eshop.model.Order;
 import id.ac.ui.cs.advprog.eshop.service.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 @Controller
+@RequestMapping("/order")
 public class OrderController {
 
     @Autowired
     private OrderService orderService;
+
+    @GetMapping("/create")
+    public String createOrderPage() {
+        return "createOrder";
+    }
+
+    @GetMapping("/history")
+    public String orderHistoryPage() {
+        return "orderHistoryForm";
+    }
+
+    @PostMapping("/history")
+    public String orderHistoryByAuthor(
+            @RequestParam("author") String author,
+            Model model
+    ) {
+        List<Order> orders = orderService.findAllByAuthor(author);
+        model.addAttribute("author", author);
+        model.addAttribute("orders", orders);
+        return "orderHistoryList";
+    }
 }

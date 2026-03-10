92c9d47b83e387d50e31a06ebfb50df8151be687
hasanul muttaqin <hasanul.muttaqin@ui.ac.id>
2026-03-10 20:36:36 +0700

    [RED] Add OrderController skeleton

---
+++ src/main/java/id/ac/ui/cs/advprog/eshop/controller/OrderController.java
@@ -0,0 +1,12 @@
+package id.ac.ui.cs.advprog.eshop.controller;
+
+import id.ac.ui.cs.advprog.eshop.service.OrderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class OrderController {
+
+    @Autowired
+    private OrderService orderService;
+}
